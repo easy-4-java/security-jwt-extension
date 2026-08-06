10a76cde9592234f64a51929f68ede93d0789d76
wandl-6A72h <hiwepy@gmail.com>
2026-08-06 14:59:56 +0800

    chore: 注释作者信息统一为 @Loong Wan

--- src/main/java/org/springframework/security/boot/JwtAuthorizationToken.java
+++ src/main/java/org/springframework/security/boot/JwtAuthorizationToken.java
@@ -7,7 +7,7 @@ import org.springframework.security.core.GrantedAuthority;
 
 /**
  * Jwt授权 (authorization) Token
- * @author ： <a href="https://github.com/hiwepy">hiwepy</a>
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 @SuppressWarnings("serial")
 public class JwtAuthorizationToken extends AbstractAuthenticationToken {
