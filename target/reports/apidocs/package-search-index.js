353a84a95c413a337daee68fa0d30367c01bc274
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-20 10:26:16 +0800

    chore: remove target/ from git index, add to .gitignore

--- target/reports/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"org.springframework.security.boot.jwt.authentication"},{"l":"org.springframework.security.boot.jwt.authentication.server"},{"l":"org.springframework.security.boot.jwt.endpoint"}];updateSearchResults();
